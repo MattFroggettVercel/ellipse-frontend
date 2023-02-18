cd5a7881b6b8375ba5133b4547fbc3ec56e36d02
Matt Froggett <matt.froggett@vercel.com>
2023-02-18 14:44:46 +0000

    Commerce

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Image from 'next/image'
 
 export default function RootLayout({
   children,
@@ -7,12 +8,38 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="absolute inset-x-0 p-14 z-10">
+          <nav className="flex items-center justify-between text-grey">
+            <ul className="flex flex-1 self-stretch items-center">
+              <li><a className="p-5" href="#">Cases</a></li>
+              <li><a className="p-5" href="#">Something</a></li>
+            </ul>
+            <div className="flex">
+              <Image
+                src="/logo.svg"
+                alt="Ellipse Logo"
+                width={200}
+                height={57}
+                priority
+              />
+            </div>
+            <ul className="flex flex-1 justify-end">
+              <li>
+                <Image
+                  src="/icons/bag.svg"
+                  alt="Ellipse Logo"
+                  width={40}
+                  height={40}
+                  priority
+                />
+              </li>
+            </ul>
+          </nav>
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,85 +6,46 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by  editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+    <main>
+      <div className="h-screen relative">
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/splash.jpg"
+          fill
+          alt={""}
+          className="object-cover"
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      </div>
+      <div className="m-auto grid gap-10 grid-cols-2 max-w-5xl my-20">
+        <div className="flex flex-col items-center">
+          <Image
+            src="/case.jpg"
+            width="298"
+            height="418"
+            alt=""
+            className="object-cover"
+          />
+
+          Liam's Gift Case
+        </div>
+        <div className="flex flex-col items-center">
+          <Image
+            src="/case.jpg"
+            width="298"
+            height="418"
+            alt=""
+            className="object-cover"
+          />
+
+          Liam's Gift Case
         </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="h-screen relative">
+        <Image
+          src="/splash2.jpg"
+          fill
+          alt={""}
+          className="object-cover"
+        />
       </div>
     </main>
   )
